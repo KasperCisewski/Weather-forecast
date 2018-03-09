773ad6d2cfdb576383091b18e37e6fdc9f28906b
Kasper Cisewski <kaspercisewski@gmail.com>
2018-03-09 15:40:38 +0100

    Done App

---
+++ MVVM_JsonRest/MVVM_JsonRest/Model/WeatherResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_JsonRest.Model
+{
+    public class WeatherResult
+    {
+        public WeatherObservation WeatherObservation { get; set; }
+    }
+}
---
+++ MVVM_JsonRest/MVVM_JsonRest/View/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM_JsonRest.View
+{
+	public partial class MainPage : ContentPage
+	{
+        ViewModel.MainPageViewModel vm;
+		public MainPage()
+		{
+            vm = new ViewModel.MainPageViewModel();
+            BindingContext = vm;
+            InitializeComponent();
+        }
+        public async void OnClicked(object o, EventArgs e)
+        {
+
+            var longitude = double.Parse(Lon.Text);
+            var latitude = double.Parse(Lat.Text);
+
+
+            // var url = string.Format("https://api.geonames.org/findNearByWeatherJSON?formatted=true&lat={0}&lng={1}&username=demo&style=full",latitude,longitude);
+            // I'dont have a access to geonames, so I can`t use my Entry values.
+            var url = string.Format("http://api.geonames.org/findNearByWeatherJSON?formatted=true&lat=42&lng=-2&username=demo&style=full"); 
+
+            await vm.GetWeatherAsync(url);
+        }
+    }
+}
